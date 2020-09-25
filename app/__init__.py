4defd518b0b11dcda1bb367c043ce725cdec98ab
Alexotieno1717 <alexotieno900@gmail.com>
2020-09-25 21:28:16 +0300

    install flask bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,20 @@
 from flask import Flask
 from config import config_options
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
 
 
 def create_app(config_name):
     app = Flask(__name__)
 
     app.config.from_object(config_options.get(config_name))
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
 
     from app.main import main
     # from app.auth import auth
