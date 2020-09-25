f44b003cee353faf043c7d334f08d97b0fb0fe4e
Alexotieno1717 <alexotieno900@gmail.com>
2020-09-25 21:49:23 +0300

    Added migration

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 
-class User(UserMixin, db.Model):
+
+class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -9,3 +11,16 @@ class User(UserMixin, db.Model):
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
+
+    pass_secure = db.Column(db.String(255))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.pass_secure, password)
