7de40fea4cb8421bf131c7a18dfb6ffaa3cedeac
Alexotieno1717 <alexotieno900@gmail.com>
2020-09-25 22:22:54 +0300

    Install flask uploads

--- app/models.py
+++ app/models.py
@@ -13,8 +13,7 @@ class User(UserMixin, db.Model):
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
-
-    pass_secure = db.Column(db.String(255))
+    password_secure = db.Column(db.String(255))
 
     @property
     def password(self):
